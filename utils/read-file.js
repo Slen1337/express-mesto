b66eb5ff9595542c5e2c9dd552f3674776b60d3b
Dmitry Nikonov <saylen211@gmail.com>
2021-01-24 20:20:44 +0300

    again updated after review

--- utils/read-file.js
+++ utils/read-file.js
@@ -1,17 +1,10 @@
 const fsPromises = require('fs').promises;
 
 const readFiles = (pathToFile) => fsPromises.readFile(pathToFile, { encoding: 'utf8' })
-  .catch(() => {
-    const fileError = { message: `file ${pathToFile} not found` };
-    throw fileError;
-  })
-  .then((data) => {
-    try {
-      return JSON.parse(data);
-    } catch (_) {
-      const jsonError = { message: 'JSON file is not working' };
-      throw jsonError;
-    }
+  .then((data) => JSON.parse(data))
+  .catch((err) => {
+    console.log(err.message);
+    throw err;
   });
 
 module.exports = readFiles;
